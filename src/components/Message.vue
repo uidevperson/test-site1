ca1a1f76c38cd6021c9e53765cd5bcf17706fc92
uidevperson <uidevthing@gmail.com>
2017-10-12 19:47:57 +0100

    set up first cue loader component

---
+++ src/components/Message.vue
@@ -0,0 +1,22 @@
+<template>
+	<div class="box">
+		<p>
+			<slot></slot>
+		</p>
+	</div>
+</template>
+
+<script>
+	export default {
+
+	}
+</script>
+
+<style>
+.box {
+	background: silver;
+	padding: 6em;
+	border: 1em solid #666;
+	margin-bottom: 1em;
+}
+</style>
