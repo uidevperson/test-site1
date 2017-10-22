d1faa1b5e44a982b1396d69e06dfc82f153e0efe
uidevperson <uidevthing@gmail.com>
2017-10-22 15:03:43 +0100

    testing

--- src/main.js
+++ src/main.js
@@ -1,46 +1,34 @@
 import Vue from 'vue'
 import axios from 'axios';
 
-Vue.component('tabs', {
-	props: ['title', 'body'],
-	template: `
-	<div>
-	  <div class="tabs">
-		  <ul>
-		  	pants
-		  </ul>
-		</div>
-	</div>
-	`,
-	data() {
-		return { 
-			tabs:[] 
-		};
-	}
-});
+// Vue.component('tabs', {
+// 	props: ['title', 'body'],
+// 	template: `
+// 	<div>
+// 	  <div class="tabs">
+// 		  <ul>
+// 		  	pants
+// 		  </ul>
+// 		</div>
+// 	</div>
+// 	`,
+// 	data() {
+// 		return { 
+// 			tabs:[] 
+// 		};
+// 	}
+// });
 // import App from './App.vue'
 
 new Vue({
   el: '#root',
   data:{
-		couponApplied: false,
+		skills: [],
 		value: ''
-	},
-	methods: { 	
-		onCouponApplied(value){
-			this.couponApplied = true;
-			this.value = value;
-		}
 	},
 	mounted(){
   	// when mounted
   	axios.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
-	  .then(function (response) {
-	    var test = JSON.stringify(response.data[0]);
-	    console.log(test);
-	  })
-	  .catch(function (error) {
-	    console.log(error);
-	  });
+	  .then(response => this.skills = response.data[0])
   },
 })
