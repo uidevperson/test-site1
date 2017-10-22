51ecce72c9a68060e37993ad783a571c39172269
uidevperson <uidevthing@gmail.com>
2017-10-22 14:57:48 +0100

    testing

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,13 @@
 <template>
   <div id='app'>
-    <message v-text="test"></message>
-    <message>Hello There</message>
-    <message>Hello There</message>
-    <message>Hello There</message>
+    {{test}}
   </div>
 </template>
 
 <script>
-  import Message from "./components/Message.vue";
+
   export default {
     name: 'app',
-    components: { Message },
-    data () {
-      return {
-        'test': 'qweqwe'
-      }
-    }
   }
 </script>
 
-<style type="text/css">
-  
-</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,46 @@
 import Vue from 'vue'
-import App from './App.vue'
+import axios from 'axios';
+
+Vue.component('tabs', {
+	props: ['title', 'body'],
+	template: `
+	<div>
+	  <div class="tabs">
+		  <ul>
+		  	pants
+		  </ul>
+		</div>
+	</div>
+	`,
+	data() {
+		return { 
+			tabs:[] 
+		};
+	}
+});
+// import App from './App.vue'
 
 new Vue({
-  el: '#app',
-  render: h => h(App)
+  el: '#root',
+  data:{
+		couponApplied: false,
+		value: ''
+	},
+	methods: { 	
+		onCouponApplied(value){
+			this.couponApplied = true;
+			this.value = value;
+		}
+	},
+	mounted(){
+  	// when mounted
+  	axios.get('http://ron-swanson-quotes.herokuapp.com/v2/quotes')
+	  .then(function (response) {
+	    var test = JSON.stringify(response.data[0]);
+	    console.log(test);
+	  })
+	  .catch(function (error) {
+	    console.log(error);
+	  });
+  },
 })
